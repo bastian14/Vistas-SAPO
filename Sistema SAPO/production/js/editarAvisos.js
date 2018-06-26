af32f337434587ba7272a551c78ade18efaa1fde
Bescalona <bescaubb@gmail.com>
2018-06-26 15:12:33 -0400

    asdas

--- Sistema SAPO/production/js/editarAvisos.js
+++ Sistema SAPO/production/js/editarAvisos.js
@@ -195,7 +195,7 @@ function subirAvisoEditado(){
               hideAfter : 2000,              // `false` to make it sticky or time in miliseconds to hide after
               stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
               textAlign : 'left',            // Alignment of text i.e. left, right, center
-              position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+              position : 'top-right'         // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
             })
             $("#contenido").load("avisosAdmin.html");
             //document.location="indexAdmin.html";
