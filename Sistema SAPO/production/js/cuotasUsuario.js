7fb7d86c0d89ca8814192e64662e204365dd0afb
Basthian <basthian.henriquez1401@alumnos.ubiobio.cl>
2018-07-04 02:58:34 -0400

    Implementación Vistas de Apoderado (Funcional)
    Avisos, acuerdos, anotaciones, historial de cuotas y notas parciales.
    +JS faltantes

--- Sistema SAPO/production/js/cuotasUsuario.js
+++ Sistema SAPO/production/js/cuotasUsuario.js
@@ -1,42 +1,30 @@
-function cargarAcuerdos(){
+function cargarCuotasUsuario() {
   var email = localStorage.getItem("email");
   var pass = localStorage.getItem("pass");
   if(email !== null && pass !== null){
-
     $.ajax({
-        url: 'http://sapo2018.000webhostapp.com/mostrarAcuerdosApoderados.php',
-        method: 'POST',
-        dataType: 'json',
-        data: {
-        email: email,
-        pass: pass
+      url: 'http://sapo2018.000webhostapp.com/mostrarCuotasDetallesApoderados.php',
+      method: 'POST',
+      dataType: 'json',
+      data: {
+      email: email,
+      pass: pass
       },
       success: function(data){
         if(data.resp){
-          var profesor;
-          var curso;
-          var fecha;
-          var contenido;
-          var idAcuerdo;
+          var cont = 0;
           var cantidad = data.cantidad;
 
-          for (var i = 0; i < cantidad; i++) {
-            profesor = data.datos[i].profesor;
-            curso = data.datos[i].curso;
-            fecha = data.datos[i].fecha;
-            contenido = data.datos[i].contenido;
-            idAcuerdo = data.datos[i].idAcuerdo;
-            console.log("profesor: "+profesor+" curso: "+curso+" fecha: "+fecha+" contenido: "+contenido);
+          $.each( data.datos, function( key, value ) {
+            cont++;
             text_html ='<tr>';
-            text_html +='<td>'+profesor+'</td>';
-            text_html +='<td>'+curso+'</td>';
-            text_html +='<td>'+fecha+'</td>';
-            text_html +='<td>'+contenido+'</td>';
-            //text_html +='<td><button onclick="editarAcuerdos(\''+idAcuerdo+'\')" type="button" class="btn btn-primary">Editar</button></td>';
-            //text_html +='<td><button onclick="borrarAcuerdos(\''+idAcuerdo+'\')" type="button" class="btn btn-primary">Eliminar</button></td>';
-            text_html +='<tr>';
-            $('#tablaAcuerdos').append(text_html);
-          }
+            text_html +='<td>'+cont+'</td>';
+            text_html +='<td>'+value.mes+'</td>';
+            text_html +='</tr>';
+            $('#tablaCuotas').append(text_html);
+          });
+
+
         }else{
           //alert('ERROR');
           $.toast({
