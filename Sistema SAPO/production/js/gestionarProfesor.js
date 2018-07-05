88e016a66197f807e8c1972e7b4a8f713a922d3d
Cristobal Sandoval <cristobalsgutierrez@gmail.com>
2018-07-05 04:22:48 -0400

    implementación de gestionar apoderados y profesores
    al 100 ambas

--- Sistema SAPO/production/js/gestionarProfesor.js
+++ Sistema SAPO/production/js/gestionarProfesor.js
@@ -234,8 +234,8 @@ function cargarProfesoresGestionar() {
             }*/
             //apoderado = value.nombreApoderado+" "+value.apellidoApoderado;
 
-            text_html +='<td><button onclick="editarApoderadoGestionar(\''+value.idProfesor+'\');" type="button" class="btn btn-warning">Editar</button></td>';
-            text_html +='<td><button onclick="eliminarApoderadoGestionar(\''+value.idProfesor+'\');" type="button" class="btn btn-danger">Eliminar</button></td>';
+            text_html +='<td><button onclick="editarProfesorGestionar(\''+value.idProfesor+'\');" type="button" class="btn btn-warning">Editar</button></td>';
+            text_html +='<td><button onclick="eliminarProfesorGestionar(\''+value.idProfesor+'\');" type="button" class="btn btn-danger">Eliminar</button></td>';
             text_html +='</tr>';
             $('#tablaGProfesor').append(text_html);
           });
@@ -292,14 +292,14 @@ function cargarProfesoresGestionar() {
 
 //------------- EDITAR APODERADO----------------------
 
-function eliminarApoderadoGestionar(id){
+function eliminarProfesorGestionar(id){
   console.log(id);
   var email = localStorage.getItem("email");
   var pass = localStorage.getItem("pass");
   if(confirm("desea continuar")){
     if(email !== null && pass !== null){
       $.ajax({
-        url: 'http://sapo2018.000webhostapp.com/eliminarApoderado.php',
+        url: 'http://sapo2018.000webhostapp.com/eliminarProfesor.php',
         method: 'POST',
         dataType: 'json',
         data: {
@@ -311,7 +311,7 @@ function eliminarApoderadoGestionar(id){
           if(data.resp){
             //alert("Acuerdo eliminado con exito.");
             $.toast({
-              text : "<h2>Apoderado eliminado con exito</h2>",
+              text : "<h2>Profesor eliminado con exito</h2>",
               showHideTransition : 'slide',  // It can be plain, fade or slide
               icon: 'success',
               bgColor : 'dark-red',              // Background color for toast
@@ -322,7 +322,7 @@ function eliminarApoderadoGestionar(id){
               textAlign : 'left',            // Alignment of text i.e. left, right, center
               position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
             });
-            $("#contenido").load("gestionarApoderado.html");
+            $("#contenido").load("gestionarProfesor.html");
           }else{
             //alert('ERROR');
             $.toast({
@@ -375,35 +375,32 @@ function eliminarApoderadoGestionar(id){
 }
 
 
-function editarApoderadoGestionar(id){
-  localStorage.setItem("idApoderadoEdit", id);
-  $("#contenido").load("editarApoderado.html");
+function editarProfesorGestionar(id){
+  localStorage.setItem("idProfesorEdit", id);
+  $("#contenido").load("editarProfesor.html");
 }
 
-function cargarAlumnosEditarApoderado() {
-  if(localStorage.getItem("email") !== null && localStorage.getItem("pass") !== null && localStorage.getItem("idApoderadoEdit") !== null){
+function cargarCursosEditarProfesor() {
+  if(localStorage.getItem("email") !== null && localStorage.getItem("pass") !== null){
     var email = localStorage.getItem("email");
     var pass = localStorage.getItem("pass");
-    var idApo = localStorage.getItem("idApoderadoEdit");
+    console.log(email+ " " + pass);
     $.ajax({
-      url: 'http://sapo2018.000webhostapp.com/seleccionarAlumnoEditarApo.php',
+      url: 'http://sapo2018.000webhostapp.com/seleccionarCurso2.php',
       method: 'POST',
       dataType: 'json',
       data: {
       email: email,
-      idApo: idApo,
       pass: pass
       },
       success: function(data){
         if(data.resp){
-
           $.each( data.datos, function( key, value ) {
             console.log(value);
-            text_html ='<option value="'+value.idAlumno+'">'+value.alumnoNombre+" "+value.alumnoApellido+'</option>';
-            $('#alumnosEditarApoderado').append(text_html);
+            text_html ='<option value="'+value.idCurso+'">'+value.grado+'</option>';
+            $('#cursosEditarProfesor').append(text_html);
           });
-          cargarInfoEditarApoderado();
-          //$('#cursos').append(text_html);
+          cargarInfoEditarProfesor();
         }else{
           //alert('ERROR');
           $.toast({
@@ -433,7 +430,7 @@ function cargarAlumnosEditarApoderado() {
           stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
           textAlign : 'left',            // Alignment of text i.e. left, right, center
           position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
-      });
+        });
       }
     });
   }else {
@@ -449,39 +446,40 @@ function cargarAlumnosEditarApoderado() {
       stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
       textAlign : 'left',            // Alignment of text i.e. left, right, center
       position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
-  });
+    });
     document.location="login.html"
   }
 }
 
-function cargarInfoEditarApoderado() {
+function cargarInfoEditarProfesor() {
   var email = localStorage.getItem("email");
   var pass = localStorage.getItem("pass");
-  var idApo = localStorage.getItem("idApoderadoEdit");
-  if(email !== null && pass !== null && idApo != null){
+  var idPro = localStorage.getItem("idProfesorEdit");
+  if(email !== null && pass !== null && idPro != null){
 
     $.ajax({
-      url: 'http://sapo2018.000webhostapp.com/editarApoderado.php',
+      url: 'http://sapo2018.000webhostapp.com/editarProfesor.php',
       method: 'POST',
       dataType: 'json',
       data: {
       email: email,
-      idApo: idApo,
+      idPro: idPro,
       pass: pass
       },
       success: function(data){
         if(data.resp){
-          var nombreApoEdit = data.datos.nombreApoderado;
-          var apellidoApoEdit = data.datos.apellidoApoderado;
-          var idAlumnoEdit = data.datos.idAlumno;
-          var correoApoEdit = data.datos.correoApoderado;
-          var passApoEdit = data.datos.pApoderado;
+          var nombreProEdit = data.datos.nombrePro;
+          var apellidoProEdit = data.datos.apellidoPro;
+          var idCursoEdit = data.datos.idCursoPro;
+          var correoProEdit = data.datos.correoPro;
+          var passProEdit = data.datos.pPro;
           //console.log("titulo: "+titulo+"idcurso: "+curso+"fecha: "+fecha+"contenido: "+contenido);
-          $('#nombreApoderado').val(nombreApoEdit);
-          $('#apellidoApoderado').val(apellidoApoEdit);
-          $('#alumnosEditarApoderado').val(idAlumnoEdit);
-          $('#correoApoderado').val(correoApoEdit);
-          $('#contraseñaApoderado').val(passApoEdit);
+          $('#nombrePro').val(nombreProEdit);
+          $('#apellidoPro').val(apellidoProEdit);
+          //$('#cursosEditarProfesor').val(idCursoEdit);
+          document.ready = document.getElementById("cursosEditarProfesor").value = idCursoEdit;
+          $('#correoPro').val(correoProEdit);
+          $('#contraseñaPro').val(passProEdit);
 
         }else{
           //alert('ERROR');
@@ -531,4 +529,130 @@ function cargarInfoEditarApoderado() {
         })
     document.location="login.html"
   }
+}
+
+function editarProfesorSubirInfoGestionar() {
+
+  var nombrePro = $("#nombrePro").val();
+  var apellidoPro = $("#apellidoPro").val();
+  var cursoPro = $("#cursosEditarProfesor").val();
+  var correoPro = $("#correoPro").val();
+  var passPro = $("#contraseñaPro").val();
+  //var passApo2 = $("#contraseñaApoderado2").val();
+
+  var email = localStorage.getItem("email");
+  var pass = localStorage.getItem("pass");
+  var idUsuarioEdit = localStorage.getItem("idProfesorEdit");
+
+  console.log("guardo los datos");
+  console.log("nombre: "+nombrePro+"\napellido: "+apellidoPro+"\ncurso: "+cursoPro+"\ncorreo: "+correoPro+"\npass: "+passPro);
+  var data = new FormData();
+
+  data.append("idUsuarioEdit", idUsuarioEdit);
+  data.append("nombrePro", nombrePro);
+  data.append("apellidoPro",apellidoPro);
+  data.append("cursoPro", cursoPro);
+  data.append("correoPro",correoPro);
+  data.append("passPro", passPro);
+  data.append("email",email);
+  data.append("pass", pass);
+
+  if(email !== null && pass !== null){
+    if (cursoPro != "noSeleccionado" && nombrePro.length != 0 && apellidoPro.length != 0 && correoPro.length != 0 && passPro.length != 0) {
+      //console.log(data);
+      $.ajax({
+        url: 'https://sapo2018.000webhostapp.com/editarInfoProfesor.php',
+        method: 'POST',
+        dataType: 'json',
+        contentType: false,
+        data: data,
+        processData: false,
+        cache: false,
+        success: function(data){
+          console.log("good");
+          if(data.resp){
+            //alert("ACUERDO INGRESADO CON EXITO");
+            console.log("success");
+              $.toast({
+                text : "<h2>PROFESOR EDITADO CON EXITO</h2>",
+                showHideTransition : 'slide',  // It can be plain, fade or slide
+                icon: 'success',
+                bgColor : 'dark-red',              // Background color for toast
+                textColor : '#eee',            // text color
+                allowToastClose : false,       // Show the close button or not
+                hideAfter : 2000,              // `false` to make it sticky or time in miliseconds to hide after
+                stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                textAlign : 'left',            // Alignment of text i.e. left, right, center
+                position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+              });
+            $("#contenido").load("gestionarProfesor.html");
+            //document.location="indexAdmin.html";
+             //cargar('acuerdosTomadosAdmin');
+            }else{
+              console.log("error 1");
+              console.log("Resp. SER: "+data.resp2);
+              //alert("ERROR");
+              $.toast({
+                  text : "<h2>ERROR</h2>",
+                  showHideTransition : 'slide',  // It can be plain, fade or slide
+                  icon: 'error',
+                  bgColor : 'dark-red',              // Background color for toast
+                  textColor : '#eee',            // text color
+                  allowToastClose : false,       // Show the close button or not
+                  hideAfter : 2000,              // `false` to make it sticky or time in miliseconds to hide after
+                  stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+                  textAlign : 'left',            // Alignment of text i.e. left, right, center
+                  position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+               });
+            }
+          },
+          error: function(){
+            //alert("EL WS NO RESPONDE");
+            console.log("error 2");
+            $.toast({
+              text : "<h2>EL WS NO RESPONDE</h2>",
+              showHideTransition : 'slide',  // It can be plain, fade or slide
+              icon: 'warning',
+              bgColor : 'dark-red',              // Background color for toast
+              textColor : '#eee',            // text color
+              allowToastClose : false,       // Show the close button or not
+              hideAfter : 2000,              // `false` to make it sticky or time in miliseconds to hide after
+              stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+              textAlign : 'left',            // Alignment of text i.e. left, right, center
+              position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+            });
+          }
+        });
+        console.log("Post ajax");
+      }else {
+        //alert('DEBE LLENAR TODOS LOS CAMPOS DE INFORMACION');
+        $.toast({
+          text : "<h2>DEBE LLENAR TODOS LOS CAMPOS DE INFORMACION</h2>",
+          showHideTransition : 'slide',  // It can be plain, fade or slide
+          icon: 'warning',
+          bgColor : 'dark-red',              // Background color for toast
+          textColor : '#eee',            // text color
+          allowToastClose : false,       // Show the close button or not
+          hideAfter : 2000,              // `false` to make it sticky or time in miliseconds to hide after
+          stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+          textAlign : 'left',            // Alignment of text i.e. left, right, center
+          position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+        });
+      }
+    }else {
+      //alert('DEBE INICIAR SESIÓN');
+      $.toast({
+        text : "<h2>DEBE INICIAR SESIÓN</h2>",
+        showHideTransition : 'slide',  // It can be plain, fade or slide
+        icon: 'warning',
+        bgColor : 'dark-red',              // Background color for toast
+        textColor : '#eee',            // text color
+        allowToastClose : false,       // Show the close button or not
+        hideAfter : 2000,              // `false` to make it sticky or time in miliseconds to hide after
+        stack : 1,                     // `fakse` to show one stack at a time count showing the number of toasts that can be shown at once
+        textAlign : 'left',            // Alignment of text i.e. left, right, center
+        position : 'top-right'       // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+      });
+      document.location="login.html"
+    }
 }
